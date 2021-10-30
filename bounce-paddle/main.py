2f169ce26a810a403d9c1383969217a40a2170a2
Zach <zap122333@gmail.com>
2021-10-30 08:18:48 -0500

    Add bounce-paddle boilerplate.

---
+++ bounce-paddle/main.py
@@ -0,0 +1,13 @@
+import pygame
+
+class App:
+
+    def __init__(self) -> None:
+        pass
+    
+    def main(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    app = App()
+    app.main()
